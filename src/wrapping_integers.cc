897da1c6640c52a90f5cf658e9e720b3c901a68c
seven <seve_tis@proton.me>
2025-02-20 19:02:45 +0800

    Implement Wrap32

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,18 +1,20 @@
 #include "wrapping_integers.hh"
-#include "debug.hh"
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return zero_point + n;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  const uint64_t bin_32 = static_cast<uint64_t>(UINT32_MAX) + 1;
+  uint64_t cur_point = (raw_value_ - zero_point.raw_value_ + bin_32) % bin_32;
+  if (cur_point < checkpoint) {
+    cur_point += bin_32 * ((checkpoint - cur_point) / bin_32);
+    if (checkpoint - cur_point > bin_32 / 2)
+      cur_point += bin_32;
+  }
+  return cur_point;
 }
